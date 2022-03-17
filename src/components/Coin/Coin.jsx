0a4dcb6acd6ba11bf8e709050184cbc5222bc573
Sara Chandlee <sara@sarachandlee.com>
2022-03-17 15:21:31 -0400

    lifting state up

--- src/components/Coin/Coin.jsx
+++ src/components/Coin/Coin.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 
 const Td = styled.td`
   border: 1px solid #ccc;
@@ -10,26 +10,24 @@ const Td = styled.td`
 export default class Coin extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      price: this.props.price
-    }
     this.handleClick = this.handleClick.bind(this);
   }
   handleClick(event) {
     event.preventDefault();
-    const randomPercentage = 0.995 + Math.random() * 0.01;
-    this.setState( function(oldState) {
-      return {
-        price: oldState.price * randomPercentage
-      }
-    });
+    this.props.handleRefresh(this.props.ticker);
+    // const randomPercentage = 0.995 + Math.random() * 0.01;
+    // this.setState( function(oldState) {
+    //   return {
+    //     price: oldState.price * randomPercentage
+    //   }
+    // });
   }
   render() {
     return (
         <tr>
             <Td>{this.props.name}</Td>
             <Td>{this.props.ticker}</Td>
-            <Td>${this.state.price}</Td>
+            <Td>${this.props.price}</Td>
             <Td>
               <button onClick={this.handleClick}>Refresh</button>
             </Td>
